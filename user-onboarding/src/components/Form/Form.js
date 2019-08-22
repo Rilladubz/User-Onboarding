f9b0eb7c527f53cbbe1dcb4e7f647d6441141877
Samuel Torres <rilla@Samuels-iMac.local>
2019-08-22 18:00:37 -0400

    Step 3 complete

--- user-onboarding/src/components/Form/Form.js
+++ user-onboarding/src/components/Form/Form.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Field, withFormik } from 'formik';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
 // name, email, password, terms of service (checkbox), Submit button
 // 
 
-const UserForm = ({  }) => {
+const UserForm = ({ errors, touched, values, status }) => {
+
+    const [users, setUsers] = useState([]);
+    console.log('touched:', touched);
+    useEffect(() =>{
+        if (status){
+            setUsers([...users, status]);
+        }
+    }, [status]);
+
     return(
         <div>
             <h1>User Onboarding</h1>
             <Formik>
                 <Form>
                     <Field type="text" name="name" placeholder="Name" />
+                    {touched.name && errors.name &&(
+                        <p className="error">{errors.name}</p>
+                    )}
                     <Field type="text" name="email" placeholder="Email"/>
+                    {touched.email && errors.email &&(
+                        <p className="error">{errors.email}</p>
+                    )}
                     <Field type="text" name="password" placeholder="Password" />
-                    <Field type="checkbox" name="Tos" placeholder="Terms of Service" />
+                    {touched.password && errors.password &&(
+                        <p className="error">{errors.password}</p>
+                    )}
+
+                    <Field type="checkbox" name="tos" checked={values.tos} />
                     <button type="submit">Submit</button>
                 </Form>
             </Formik>
+
+            {users.map(user => (
+                <ul key={user.id}>
+                    <li>name: {user.name}</li>
+                    <li>email: {user.email}</li>
+                    <li>password: {user.password}</li>
+                </ul>
+            ))}
         </div>
     );
 };
@@ -35,12 +63,21 @@ const FormikUserForm = withFormik({
     },
 
     validationSchema: Yup.object().shape({
-        name: Yup.string().required(),
-        email: Yup.string().required(),
-        password: Yup.string().required(),
+        name: Yup.string().required('Name is required!'),
+        email: Yup.string().required('Email is required!'),
+        password: Yup.string().required('password is required!'),
         
-    })
+    }),
 
-})
+    handleSubmit(values, { setStatus }){
+        axios
+        .post('https://reqres.in/api/users', values)
+        .then(res =>{
+            console.log('ax response:', res)
+            setStatus(res.data);
+        })
+        .catch(err => console.log(err.response));
+    }
+})(UserForm);
 
-export default UserForm;
+export default FormikUserForm;
